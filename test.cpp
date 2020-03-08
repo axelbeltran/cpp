70115c7e4668148df62d9001b796e4beda39f4b4
axelbeltran <iscaxelbeltran@gmail.com>
2020-03-08 13:39:31 -0600

    Test of cpp

---
+++ test.cpp
@@ -0,0 +1,13 @@
+//input and output library
+#include <iostream>
+
+// space std to using the function in this space
+using namespace std;
+
+// principal function
+int main(){
+	// fuction output  Message  end line
+	cout << "Hola Mundo" << endl;
+	//the return of the principal function 
+	return 0;
+}
